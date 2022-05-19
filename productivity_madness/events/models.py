59b9cdbd65b01d7f81c29fb56c58dd5cbb9f571f
inarvos <inarvos@gmail.com>
2022-05-19 20:08:44 +0100

    Created model Event, and added EventAdmin.

--- productivity_madness/events/models.py
+++ productivity_madness/events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    repeatable = models.CharField(max_length=20, choices = [
+        ('not_repeatable', 'Not repeatable'),
+        ('once', 'Once'),
+        ('daily', 'Daily'),
+        ('weekly', 'Weekly'),
+        ('monthly', 'Monthly'),
+        ('yearly', 'Yearly')])
+
+    def __str__(self):
+        return "Event(name={}, repeatable={})".format(self.name, self.repeatable)
