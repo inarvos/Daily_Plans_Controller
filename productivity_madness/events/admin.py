3a1ce25c8360eb7bc95fb827993ea2f89e3e8767
Volodymyr Hromyk <37037175+inarvos@users.noreply.github.com>
2022-05-25 20:16:26 +0100

    Delete productivity_madness directory

--- productivity_madness/events/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Event
-
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
-    fields = ('name', 'repeatable', 'description', 'start_date', 'duration') 
-    list_display = ['id', 'name', 'repeatable', 'start_date', 'duration', 'end_date']
