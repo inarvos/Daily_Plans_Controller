82b4f862895e82679645ec625c47a8652b989f18
inarvos <inarvos@gmail.com>
2022-05-28 18:39:58 +0100

    Developed Task model.

--- daily_plans_controller/events/admin.py
+++ daily_plans_controller/events/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Task
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     fields = ('name', 'repeatable', 'description', 'start_date', 'duration') 
     list_display = ['id', 'name', 'repeatable', 'start_date', 'duration', 'end_date']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', 'parent', 'deadline', 'done')
+    list_display = ['parent', 'id', 'name', 'description', 'deadline', 'done']
--- daily_plans_controller/events/models.py
+++ daily_plans_controller/events/models.py
@@ -14,7 +14,6 @@ class Event(models.Model):
         ('yearly', 'Yearly')])
     description = models.TextField(max_length=200, blank=True)
     start_date = models.DateTimeField(default=timezone.now(), blank=False)
-
     duration = models.DurationField(default=datetime.timedelta(hours=1), blank=False)
 
     @property
@@ -23,3 +22,21 @@ class Event(models.Model):
 
     def __str__(self):
         return "Event(name={}, repeatable={})".format(self.name, self.repeatable)
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    deadline = models.DateTimeField(default=timezone.now(), blank=True)
+    done = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.done:
+            children = Task.objects.filter(parent=self)
+            for child in children:
+                child.done = True
+                child.save()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"Task(name={self.name}, id={self.id})"
