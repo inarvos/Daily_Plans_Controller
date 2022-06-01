4600f7b9a398f1ae055a9e9148b13f653047af2d
inarvos <inarvos@gmail.com>
2022-06-01 01:39:21 +0100

    Extended models and properly planned future work.

--- daily_plans_controller/events/admin.py
+++ daily_plans_controller/events/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Event, Task
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', 'parent', 'deadline', 'postponed', 'reminder', 'done')
+    list_display = ['parent', 'id', 'name', 'description', 'deadline', 'postponed', 'reminder', 'done']
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     fields = ('name', 'repeatable', 'description', 'start_date', 'duration') 
-    list_display = ['id', 'name', 'repeatable', 'start_date', 'duration', 'end_date']
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    fields = ('name', 'description', 'parent', 'deadline', 'done')
-    list_display = ['parent', 'id', 'name', 'description', 'deadline', 'done']
+    list_display = ['id', 'name', 'repeatable', 'start_date', 'end_date']
--- daily_plans_controller/events/models.py
+++ daily_plans_controller/events/models.py
@@ -1,7 +1,55 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
 from django.utils import timezone
-
 import datetime
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=CASCADE)
+    deadline = models.DateTimeField(blank=True, null=True)
+    done = models.BooleanField(default=False)
+    "TODO If done -> archive"
+    done_at = models.DateTimeField(null=True)
+    archived = models.BooleanField(default=False)
+    "TODO"
+    postponed = models.CharField(default=False, blank=True, max_length=20, choices = [
+        ('1day', 'One day'),
+        ('3days', 'Three days'),
+        ('1week', 'One week'),
+        ('2weeks', 'Two weeks'),
+        ('1month', 'One month')])
+    "TODO If deadline:"
+    reminder = models.CharField(default=False, blank=True, max_length=20, choices = [
+        ('week_before', 'Week before'),
+        ('2days_before', '2 days before'),
+        ('1day_before', '1 day before'),
+        ('3hours_before', '3 hours before'),
+        ('1hour_before', '1 hour before'),
+        ('30min_before', '30 minutes before'),
+        ('15min_before', '15 minutes before')])
+
+    """
+    TODO
+    Notifications:
+        if deadline + reminder: create Notification = deadline - reminder;
+        if postponed: create Notification = Today + postpone time
+    """
+
+    def save(self, *args, **kwargs):
+        if self.done:
+            children = Task.objects.filter(parent=self)
+            for child in children:
+                child.done = True
+                child.save()
+            self.done_at = timezone.now()
+        elif not self.done and self.done_at:
+            self.done_at = None
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"Task(name={self.name}, id={self.id})"
 
 class Event(models.Model):
     name = models.CharField(max_length=50)
@@ -15,6 +63,16 @@ class Event(models.Model):
     description = models.TextField(max_length=200, blank=True)
     start_date = models.DateTimeField(default=timezone.now(), blank=False)
     duration = models.DurationField(default=datetime.timedelta(hours=1), blank=False)
+    "TODO"
+    reminder = CharField(max_length=20, choices = [
+        ('month_before', 'Month before'),
+        ('2weeks_before', '2 weeks before'),
+        ('1week_before', '1 week before'),
+        ('3days_before', '3 days before'),
+        ('1day_before', '1 day before'),
+        ('3hours_before', '3 hours before')])
+
+    "TODO Notification = start_date - reminder"
 
     @property
     def end_date(self):
@@ -22,21 +80,3 @@ class Event(models.Model):
 
     def __str__(self):
         return "Event(name={}, repeatable={})".format(self.name, self.repeatable)
-
-class Task(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
-    deadline = models.DateTimeField(default=timezone.now(), blank=True)
-    done = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        if self.done:
-            children = Task.objects.filter(parent=self)
-            for child in children:
-                child.done = True
-                child.save()
-        super().save(*args, **kwargs)
-    
-    def __str__(self):
-        return f"Task(name={self.name}, id={self.id})"
